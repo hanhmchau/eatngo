0ecfbb2acc7cfcb58ab30f0be86ed6120ec532f2
M.C <hanhchaumn@gmail.com>
2019-02-26 21:10:07 +0700

    Add snake case mappers

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,7 +1,13 @@
-const { Model } = require('objection');
+const { Model, snakeCaseMappers } = require('objection');
 const { getModelFile } = require('./helper');
 
 class Brand extends Model {
+	static get columnNameMappers() {
+		// If your columns are UPPER_SNAKE_CASE you can
+		// use snakeCaseMappers({ upperCase: true })
+		return snakeCaseMappers();
+	}
+	
 	// Table name is the only required property.
 	static get tableName() {
 		return 'brand';
